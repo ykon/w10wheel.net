a446c5bc8698b860502cf9ab0966bb17beab5292
Yuki Ono <ykon0x1@gmail.com>
2017-10-14 10:53:23 +0900

    Improve load properties

--- W10Wheel/src/Properties.fs
+++ W10Wheel/src/Properties.fs
@@ -11,74 +11,81 @@ open System.Collections.Generic
 open System.Diagnostics
 open System.Text.RegularExpressions
 
+let private propLineRegex = new Regex("^\s*\S+\s*=\s*\S+\s*$")
+
 type Properties() =
     let sdict = SortedDictionary<string, string>()
+    let trims (ss:string[]) = ss |> Array.map (fun s -> s.Trim())
 
-    member self.Load (path: string): unit =
-        let removeComment (l: string) =
-            let si = l.IndexOf('#')
-            if si = -1 then l else l.Substring(0, si)
+    member self.Load (path:string): unit =
+        let removeComment (l:string) =
+            match l.IndexOf('#') with
+            | -1 -> l
+            | n -> l.Substring(0, n)
 
-        let splitEqual (l: string) =
-            let pair: string array = l.Split('=')
-            (pair.[0].Trim(), pair.[1].Trim())
+        let isPropLine l = propLineRegex.Match(l).Success
+
+        let getKeyValue (l:string) =
+            match l.Split('=') |> trims with
+            | [|k; v|] -> (k, v)
+            | _ -> invalidArg "l" "Invalid property line"
+
+        let setDict (k, v) =
+            Debug.WriteLine(sprintf "Load property: %s = %s" k v)
+            sdict.[k] <- v
 
         File.ReadAllLines(path) |>
-        Array.map (fun l -> (removeComment l).Trim()) |>
-        Array.filter (fun l -> l <> "") |>
-        Array.map splitEqual |>
-        Array.iter (fun (k, v) -> sdict.[k] <- v)
+        Array.map removeComment |> trims |>
+        Array.filter isPropLine |> Array.map getKeyValue |>
+        Array.iter setDict
 
-    member self.Store (path: string): unit =
-        let lines =
-            sdict.Keys |> Seq.map (fun k ->
-                (sprintf "%s=%s" k sdict.[k]))
+    member self.Store (path:string): unit =
+        let makePropLine k = (sprintf "%s=%s" k sdict.[k])
+        let lines = Seq.map makePropLine sdict.Keys
         File.WriteAllLines(path, lines)
-        ()
 
-    member self.GetProperty (key: string): string =
+    member self.GetProperty (key:string): string =
         try
             sdict.[key]
         with
             | :? KeyNotFoundException as e -> raise (KeyNotFoundException(key))
 
-    member self.SetProperty (key: string, value: string) =
+    member self.SetProperty (key:string, value:string): unit =
         sdict.[key] <- value
 
     member self.Item
         with get key = self.GetProperty key
         and set key value = self.SetProperty(key, value)
 
-    member self.GetString (key: string) =
+    member self.GetString (key:string): string =
         self.GetProperty(key)
 
-    member self.GetInt (key: string): int =
+    member self.GetInt (key:string): int =
         Int32.Parse(self.GetString(key))
 
-    member self.GetDouble (key: string): double =
+    member self.GetDouble (key:string): double =
         Double.Parse(self.GetString(key))
 
-    member self.GetBool (key: string): bool =
+    member self.GetBool (key:string): bool =
         Boolean.Parse(self.GetString(key))
 
-    member self.GetArray (key: string): string array =
-        self.GetString(key).Split(',') |>
-        Array.map (fun s -> s.Trim()) |>
+    member self.GetArray (key:string): string[] =
+        self.GetString(key).Split(',') |> trims |>
         Array.filter (fun s -> s <> "")
 
-    member self.GetIntArray (key: string): int array =
+    member self.GetIntArray (key:string): int[] =
         self.GetArray(key) |> Array.map (fun s -> Int32.Parse(s))
 
-    member self.GetDoubleArray (key: string): double array =
+    member self.GetDoubleArray (key:string): double[] =
         self.GetArray(key) |> Array.map (fun s -> Double.Parse(s))
 
-    member self.SetInt (key: string, n: int) =
+    member self.SetInt (key:string, n:int): unit =
         self.SetProperty(key, n.ToString())
 
-    member self.SetDouble (key: string, d: double) =
+    member self.SetDouble (key:string, d:double): unit =
         self.SetProperty(key, d.ToString("F"))
 
-    member self.SetBool (key: string, b: bool) =
+    member self.SetBool (key:string, b:bool): unit =
         self.SetProperty(key, b.ToString())
         
 let USER_DIR = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
@@ -90,37 +97,37 @@ let DEFAULT_DEF = "Default"
 
 let private BAD_DEFAULT_NAME = (sprintf "%s.%s.%s" PROP_NAME DEFAULT_DEF PROP_EXT)
 
-let private userDefPat = new Regex(sprintf "^\.%s\.(?!--)(.+)\.%s$" PROGRAM_NAME PROP_EXT)
+let private userDefRegex = new Regex(sprintf "^\.%s\.(?!--)(.+)\.%s$" PROGRAM_NAME PROP_EXT)
 
-let private isPropFile (path: String): bool =
+let private isPropFile (path:String): bool =
     let name = Path.GetFileName(path)
-    name <> BAD_DEFAULT_NAME && userDefPat.Match(name).Success
+    name <> BAD_DEFAULT_NAME && userDefRegex.Match(name).Success
 
-let getUserDefName (path: String) =
+let getUserDefName (path:String): string =
     let name = Path.GetFileName(path)
-    userDefPat.Match(name).Groups.[1].Value
+    userDefRegex.Match(name).Groups.[1].Value
 
-let getPropFiles () =
+let getPropFiles (): string[] =
     Directory.GetFiles(USER_DIR) |> Array.filter isPropFile 
 
-let getDefaultPath () =
+let getDefaultPath (): string =
     Path.Combine(USER_DIR, DEFAULT_PROP_NAME)
 
-let getPath name =
+let getPath (name:string): string =
     if name = "Default" then
         getDefaultPath()
     else
         Path.Combine(USER_DIR, (sprintf "%s.%s.%s" PROP_NAME name PROP_EXT))
 
-let exists name =
+let exists (name:string): bool =
     File.Exists(getPath name)
 
-let copy (srcName: string) (destName: string) =
+let copy (srcName:string) (destName:string): unit =
     let srcPath = getPath(srcName)
     let destPath = getPath(destName)
 
     File.Copy(srcPath, destPath)
 
-let delete (name: string) =
+let delete (name:string): unit =
     File.Delete(getPath name)
 
